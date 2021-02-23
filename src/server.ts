218ccc1758353823f5cddffc17540da7270380ff
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2021-02-23 16:11:42 -0300

    complete second day

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
-
+import 'reflect-metadata'
 import express from 'express';
+import './database';
+import {router} from "./routes";
 
 const app = express();
 
+app.use(express.json())
+app.use(router)
 app.get("/", (request, response) => {
-    return response.json({status: 200, message: 'API runing!'})
-})
-
-app.get("/users", (request, response) => {
-    return response.json({message: "Hello World #NLW4!"})
-})
-
-app.post("/users", (request, response) => {
-    return response.json({message: "Os dados foram salvos com sucesso!"})
+    return response.status(200).json({status: 200, message: 'API runing!'})
 })
 
 app.listen(3333, () => console.log("server is runing!"))
