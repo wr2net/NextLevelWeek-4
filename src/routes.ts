29ab2caacc8162013b639d0aa842792fec7eb4a9
Wagner Rigoli da Rosa <wagner@rigolidarosa.com>
2021-02-24 16:25:17 -0300

    complete third day

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
+import { SurveyController } from "./controllers/SurveyController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveyController();
 
 router.post("/users", userController.create);
+router.get("/users", userController.show);
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export {router};
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,3 @@
-import 'reflect-metadata'
-import express from 'express';
-import './database';
-import {router} from "./routes";
-
-const app = express();
-
-app.use(express.json())
-app.use(router)
-app.get("/", (request, response) => {
-    return response.status(200).json({status: 200, message: 'API runing!'})
-})
+import {app} from "./app";
 
 app.listen(3333, () => console.log("server is runing!"))
